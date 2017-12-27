5adac4131578f2f0cead0213ded4cb69f993a96e
sincerelyyso <sobuettn@gmail.com>
2017-12-27 11:19:19 +0800

    uu

--- Group9_Analysis.R
+++ Group9_Analysis.R
@@ -1,2 +1,14 @@
 library(RCurl)
-realEstate <-read.csv(text=getURL("https://raw.githubusercontent.com/cheussernccu/nccu_data/master/Data%20Final%20Paper.csv"), header=TRUE, sep = ",")
+library(fBasics)
+realEstate <-read.csv(text=getURL("https://raw.githubusercontent.com/cheussernccu/nccu_data/master/Group9%20Data3.csv"), header=TRUE, sep = ";", stringsAsFactors = FALSE)
+#Population <-read.csv(text=getURL("https://raw.githubusercontent.com/cheussernccu/nccu_data/master/Population%20data.csv"), header=TRUE, sep = ",")
+
+realEstate.dataframe = data.frame(realEstate)
+Years=1960:1980
+for(i in Years){
+  asd  = summary(Group9_Data3[which(Group9_Data3$`Year Built` == toString(i)),])
+}
+asd = mean(Group9_Data3$`Price - Closed`)
+asd
+
+
