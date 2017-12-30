79be221382438e3d80ee387c6e86ebe0d3adea72
malte235 <malte.petry@gmx.de>
2017-12-30 17:31:30 +0800

    gh

--- Group3_Q3.R
+++ Group3_Q3.R
@@ -5,6 +5,13 @@ realEstate <-read.csv(text=getURL("https://raw.githubusercontent.com/cheusserncc
 lm(SalePrice~as.factor(YrSold),data=realEstate)
 summary(realEstate)
 
+####not working at the moment
+cmsold=c()
+for(s in realEstate){
+  cmsold=c(cmsold+((realEstate$YrSold[s]-2006)*12+realEstate$MoSold[s]))
+}
+cmsold
+
 
 #HousePriceIndex from the US for 2006-2010
 HPI <-read.csv(text=getURL("https://raw.githubusercontent.com/cheussernccu/nccu_data/master/HousePriceIndex.csv"), header=TRUE, sep = ",", stringsAsFactors = FALSE)
@@ -36,19 +43,22 @@ avgprices=head(avgprices,-5)
 IndexIowa=avgprices/avgjan06*100
 
 my
+Time=(1:55)
+Time
 
 #Plotting the values
 x11()
-plot(1:55,IndexIowa,type="l",col="red",ylim=c(0,120),xlim=c(1,55),ylab="Index",xlab="Year/Month",xaxt="n")
+plot(Time,IndexIowa,type="l",col="red",ylim=c(0,120),xlim=c(1,55),ylab="Index",xlab="Year/Month",xaxt="n")
 axis(1,at=seq(0,60,6))
 legend(1,40,legend=c("American house price index", "Iowa house prices (our dataset)"),col=c("blue","red"),lty=1)
-lines(1:55,HPI_ind,type="l",col="blue")
+lines(Time,HPI_ind,type="l",col="blue")
 lines(c(-10,100),c(100,100),lty=2)
 
 
 #Korrelation berechnen
 #x-Achse andere Beschriftungen -> Januar 2006 etc
-cor(1:55,HPI_ind)
-cor(1:55,IndexIowa)
+cor(Time,HPI_ind)
+cor(Time,IndexIowa)
 cor(HPI_ind,IndexIowa)
 cor.test(HPI_ind,IndexIowa)
+pairs(cbind(HPI_ind,IndexIowa,Time))
