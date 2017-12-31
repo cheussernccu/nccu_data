39d5660c222a132fe33bc212cba866a7334d61cb
malte235 <malte.petry@gmx.de>
2017-12-31 16:43:51 +0800

    dg

--- Group3_Q3.R
+++ Group3_Q3.R
@@ -45,22 +45,43 @@ avgprices=head(avgprices,-5)
 #Indexing our dataset to January2006
 IndexIowa=avgprices/avgjan06*100
 
+#Smoothing values by taking 3 month average
+IndexIowaS=mean(c(IndexIowa[1],IndexIowa[2]))
+for (w in 2:(length(IndexIowa)-1)){
+  tmp=mean(c(IndexIowa[w-1],IndexIowa[w],IndexIowa[w+1]))
+  IndexIowaS=c(IndexIowaS,tmp)
+}
+IndexIowaS=c(IndexIowaS,mean(c(IndexIowa[(length(IndexIowa)-1)],IndexIowa[(length(IndexIowa))])))
+
 my
 Time=(1:55)
-cbind(my,Time)
+cumo=cbind(my,Time)
 
 #Plotting the values
 x11()
-plot(Time,IndexIowa,type="l",col="red",ylim=c(0,120),xlim=c(1,55),ylab="Index",xlab="Year/Month",xaxt="n")
+plot(Time,IndexIowa,type="l",col="red",ylim=c(0,120),xlim=c(1,55),ylab="Index",xlab="Cumulated month (Starting from January 2006)",xaxt="n")
 axis(1,at=seq(0,60,6))
-legend(1,40,legend=c("American house price index", "Iowa house prices (our dataset)"),col=c("blue","red"),lty=1)
+legend(1,40,legend=c("American house price index", "Iowa house prices (our dataset)","Iowa house prices (smoothed)"),col=c("blue","red","darkseagreen"),lty=1)
 lines(Time,HPI_ind,type="l",col="blue")
+lines(Time,IndexIowaS,type="l",col="darkseagreen")
 lines(c(-10,100),c(100,100),lty=2)
 
 
-#Korrelation berechnen
-#x-Achse andere Beschriftungen -> Januar 2006 etc
+#Correlation
+#x-Axis is cummulative month (January 2006 -> 1, see "cumo")
 cor.test(Time,HPI_ind)
 cor.test(Time,IndexIowa)
+cor.test(Time,IndexIowaS)
 cor.test(HPI_ind,IndexIowa)
-pairs(cbind(HPI_ind,IndexIowa,Time))
+cor.test(HPI_ind,IndexIowaS)
+pairs(cbind(HPI_ind,IndexIowa,IndexIowaS,Time))
+
+
+#Iowa Prices also dropped due to the Real Estate Crisis. Our dataset is not big enough for a
+#representative analysis. For example the average price per month is not exactly accurate. There
+#are many factors which distort our results. Firstly, there could be exceptions from the usual 
+#price in the Iowa market which have quite a big impact on the average price in a given month. Also
+#it is possible that in a month only expensive (cheap) houses were sold which distort the average
+#in a month. So our plotted graph is not really an index.
+#huge variance
+
