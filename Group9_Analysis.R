1e96278bdbcc89ea0331c0ff315e8777a180b6ca
cheusser <caspar.heusser@aol.com>
2018-01-01 23:04:49 +0800

    Population Data import fixed

--- Group9_Analysis.R
+++ Group9_Analysis.R
@@ -1,7 +1,7 @@
 library(RCurl)
 library(fBasics)
 realEstate <-read.csv(text=getURL("https://raw.githubusercontent.com/cheussernccu/nccu_data/master/Group9%20Data3.csv"), header=TRUE, sep = ",", stringsAsFactors = FALSE)
-#Population <-read.csv(text=getURL("https://raw.githubusercontent.com/cheussernccu/nccu_data/master/Population%20data.csv"), header=TRUE, sep = ",")
+Population <-read.csv(text=getURL("https://raw.githubusercontent.com/cheussernccu/nccu_data/master/Population-data_BackUp.csv"), header=TRUE, sep = ";")
 
 realEstate.dataframe = data.frame(realEstate)
 Years=1960:2015
