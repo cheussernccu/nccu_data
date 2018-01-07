17c43e6bddf9bbf1a16716ae687ccfd18f05d39d
sincerelyyso <sobuettn@gmail.com>
2018-01-07 20:53:30 +0800

    Changes

--- Final-report_Group9.R
+++ Final-report_Group9.R
@@ -1,74 +1,59 @@
-#the estate market summarized to every year 
+#R Final paper_Group9
+
+
+#Packages
 library(RCurl)
 library(fBasics)
+
+#Data set
 realEstate <-read.csv(text=getURL("https://raw.githubusercontent.com/cheussernccu/nccu_data/master/Group9%20Data3.csv"), header=TRUE, sep = ",", stringsAsFactors = FALSE)
 realEstate.dataframe = data.frame(realEstate)
-data1 <-read.csv(text=getURL("https://raw.githubusercontent.com/cheussernccu/nccu_data/master/Population-data_BackUp.csv"), header=TRUE, sep = ";")
-rm(data1)
-#data1 <- read.csv("C:/Users/Sophie/Downloads/Population-data_BackUp (1).csv")
 
+#Adjusting the Data (categorial to numerical)
+realEstate[which(realEstate$Property.Type == "Condo"),5] = 1
+realEstate[which(realEstate$Property.Type == "Single Family"),5] = 0
 
+#The estate market summarized by every year 
 Years=1960:2015
 for(i in Years){
   print(summary(realEstate[which(realEstate$Year.Built == toString(i)),]))
 }
-realEstate[which(realEstate$Property.Type == "Condo"),5] = 1
-realEstate[which(realEstate$Property.Type == "Single Family"),5] = 0
 
-#Overview of Data
-#data1 = read.csv("C:/Users/User/Desktop/Population-data_BackUp.csv")
-
+#Create a new dummyvariable for research question 5
 data1 <- read.csv("Population-data_BackUp (1).csv", header=TRUE, sep = ";")
 for(i in (1960:2010)){
   data1[which(data1$Year == i),14] = data1[which(data1$Year == i+5),13]
 }
-#demographic data
-x11(width=10,height=8)
-par(mar=c(3,4,3,2))
-par(mfrow=c(3,2))
-plot(data1[,1],data1[,2],type="l",xlab="Year",ylab="Population",main="Total Population in New Hampshire")
-plot(data1[11:56,1],data1[11:56,3],type="l",xlab="Year",ylab="Household",main="Average Households in United States")
-plot(data1[,1],data1[,4],type="l",xlab="Year",ylab="Fertility Rate",main="Fertility Rate in United States")
-plot(data1[,1],data1[,5],type="l",xlab="Year",ylab="Birth Rate",main="Birth Rate in United States")
-plot(data1[,1],data1[,6],type="l",xlab="Year",ylab="Death Rate",main="Death Rate in United States")
-#descriptive statistics 
-overview_Pop=matrix(NA,ncol =5,nrow = 4 )
-colnames(overview_Pop)=c("Population","Average Households","Fertility Rate","Birth Rate","Death Rate")
-rownames(overview_Pop)=c("mean","maximun","minimun","standard_deviation")
-for(i in 1:5){
-  overview_Pop[1,i]=mean(data1[,i+1],na.rm = TRUE)
-}
-for(i in 1:5){
-  overview_Pop[2,i]=max(data1[,i+1],na.rm = TRUE)
-}
-for(i in 1:5){
-  overview_Pop[3,i]=min(data1[,i+1],na.rm = TRUE)
-}
-for(i in 1:5){
-  overview_Pop[4,i]=sd(data1[,i+1],na.rm = TRUE)
-}
 
-#house data
+
+#Overview of the data
+
+#House data
 x11(width=10,height=7)
 par(mar=c(3,4,2,2))
 par(mfrow=c(2,2))
+
 #House Type
 housetype=cbind(data1[,7],1-data1[,7])
 barplot(t(housetype),names.arg  = c(1960:2015),main ="Rate of House Type in New Hampshire", xlab="Year",ylab="Rate",ylim = c(0,1),legend.text =  c("Condo","Single"),col = c("lightblue","lightpink"),args.legend = list(x = "topright",cex=1,bg="white"))
+
 #Rooms
 plot(data1[1:56,1],data1[1:56,10],type="l",col="black",xlab="Year",ylab="Rooms",main="Average Rooms in New Hampshire",ylim=c(0,12),lwd=2)
 lines(data1[1:56,1],data1[1:56,8],col="red",lwd=2)
 lines(data1[1:56,1],data1[1:56,9],col="blue",lwd=2)
 legend("topright", c("Total","Bedrooms","Bathrooms"), ncol = 1, cex = 1,col=c("black","red","blue"),
        lty=c(1,1,1),lwd=c(2,2,2))
-#size
+
+#Size
 plot(data1[1:56,1],data1[1:56,12]*375.965264,type="l",col="black",xlab="Year",ylab="Size",main="Average Sizes in New Hampshire",lwd=2,ylim=c(0,13000))
 lines(data1[1:56,1],data1[1:56,11],col="red",lwd=2)
 legend("topright", c("Total","Indoor"), ncol = 1, cex = 1,col=c("black","red"),
        lty=c(1,1),lwd=c(2,2))
-#total houses
+
+#Total houses
 plot(data1[,1],data1[,13],type="l",xlab="Year",ylab="Houses",main="Total Houses in New Hampshire")
-#descriptive statistics 
+
+#Descriptive statistics 
 overview_house=matrix(NA,ncol =7,nrow = 4 )
 colnames(overview_house)=c("Rate of Condo","Bedrooms","Bathrooms","Total rooms","Indoor size(square feets)","Total size(Acres)","Total Houses")
 rownames(overview_house)=c("mean","maximun","minimun","standard_deviation")
@@ -85,9 +70,50 @@ for(i in 1:7){
   overview_house[4,i]=sd(data1[,i+6],na.rm = TRUE)
 }
 
-#regression
+
+#Demographic data
+x11(width=10,height=8)
+par(mar=c(3,4,3,2))
+par(mfrow=c(3,2))
+
+#Population
+plot(data1[,1],data1[,2],type="l",xlab="Year",ylab="Population",main="Total Population in New Hampshire")
+
+#Average size of a household
+plot(data1[11:56,1],data1[11:56,3],type="l",xlab="Year",ylab="Household",main="Average Households in United States")
+
+#Fertility rate
+plot(data1[,1],data1[,4],type="l",xlab="Year",ylab="Fertility Rate",main="Fertility Rate in United States")
+
+#Birth rate
+plot(data1[,1],data1[,5],type="l",xlab="Year",ylab="Birth Rate",main="Birth Rate in United States")
+
+#Death rate
+plot(data1[,1],data1[,6],type="l",xlab="Year",ylab="Death Rate",main="Death Rate in United States")
+
+
+#Descriptive statistics 
+overview_Pop=matrix(NA,ncol =5,nrow = 4 )
+colnames(overview_Pop)=c("Population","Average Households","Fertility Rate","Birth Rate","Death Rate")
+rownames(overview_Pop)=c("mean","maximun","minimun","standard_deviation")
+for(i in 1:5){
+  overview_Pop[1,i]=mean(data1[,i+1],na.rm = TRUE)
+}
+for(i in 1:5){
+  overview_Pop[2,i]=max(data1[,i+1],na.rm = TRUE)
+}
+for(i in 1:5){
+  overview_Pop[3,i]=min(data1[,i+1],na.rm = TRUE)
+}
+for(i in 1:5){
+  overview_Pop[4,i]=sd(data1[,i+1],na.rm = TRUE)
+}
+
+
+#Regression
 attach(data1)
-#question1
+
+#Question 1
 fit.1=lm(SingleCondo~PopTotal+Death) 
 summary(fit.1)
 library(DAAG)
@@ -156,7 +182,7 @@ summary(fit)
 fit=lm(SingleCondo~Death)
 summary(fit)
 
-#question2
+#Question 2
 fit.2=lm(TotalRooms~Avhousehold+Death, data = data1)
 summary(fit.2)
 library(olsrr)
@@ -225,7 +251,7 @@ summary(fit)
 fit=lm(TotalRooms~Death)
 summary(fit)
 
-#question3
+#Question 3
 fit.3=lm(SizeIndoor~PopTotal+Fertility+Birth+Death)
 summary(fit.3)
 
@@ -292,7 +318,7 @@ summary(fit)
 fit=lm(SizeIndoor~Death)
 summary(fit)
 
-#question4
+#Question 4
 fit.4=lm(SizeTotal~PopTotal)
 summary(fit.4)
 
@@ -359,7 +385,7 @@ summary(fit)
 fit=lm(SizeTotal~Death)
 summary(fit)
 
-#question5
+#Question 5
 #results=read.table("D:/R運算(學校)/Question 5 data.csv",sep = ",",header = TRUE)
 #attach(results)
 attach(data1)
